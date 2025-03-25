29ed25d530cf5fc1c51392504b8070f3f5d62293
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2025-03-25 15:45:17 +0000

    Add links to new googlesheet and google form inputs

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ export const GOOGLEMAP_API_KEY = process.env.REACT_APP_ALTON_GOOGLEMAP_API_KEY;
 // export const SHEET_ID = process.env.REACT_APP_ALTON_GOOGLESHEET_DATABASE_ID;
 // export const SHEET_NAME = 'Directory';
 
-export const GOOGLE_FORM_INPUT_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSdcRUOlHKa0VkSjMEudrHJUBR3y-4x6DJzNXLmrP8dtZg2Ahg/viewform";
-export const GOOGLE_FORM_SUGGESTION_LINK = "https://forms.gle/C6YjRjXJgFUuF5qA6";
+export const GOOGLE_FORM_INPUT_LINK = "https://forms.gle/TCmuW2nmhofCNECX9";
+export const GOOGLE_FORM_SUGGESTION_LINK = "https://forms.gle/kjRAjBifwTkEAzsh8";
